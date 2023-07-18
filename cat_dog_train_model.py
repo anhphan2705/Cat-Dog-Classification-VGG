aec557903156fc54686050d19ef6ce662407f720
Anh Phan <anhphanvt2705@gmail.com>
2023-07-18 10:52:04 +0700

    update file

--- cat_dog_train_model.py
+++ cat_dog_train_model.py
@@ -10,7 +10,7 @@ import time
 import os
 import copy
 
-def get_data(dir, TRAIN='train', VAL='val', TEST='test'):
+def get_data(file_dir, TRAIN='train', VAL='val', TEST='test'):
     print("[INFO] Loading data...")
     # Initialize data transformation
     data_transform = {
@@ -33,7 +33,7 @@ def get_data(dir, TRAIN='train', VAL='val', TEST='test'):
     # Initilize datasets and transform it
     datasets_img = {
         file : datasets.ImageFolder(
-            os.path.join(dir, file),
+            os.path.join(file_dir, file),
             transform=data_transform[file]
         )
         for file in [TRAIN, VAL, TEST]
@@ -49,16 +49,34 @@ def get_data(dir, TRAIN='train', VAL='val', TEST='test'):
         for file in [TRAIN, VAL, TEST]
     }
     # Show result
+    class_names = datasets_img[TRAIN].classes
     datasets_size = {file : len(datasets_img[file]) for file in [TRAIN, VAL, TEST]}
     for file in [TRAIN, VAL, TEST]:
         print(f"[INFO] Loaded {datasets_size[file]} images under {file}")
-        
-    return dataloaders, datasets_img, datasets_size
+    print(f"Classes: {class_names}")
+    
+    return datasets_img, datasets_size, dataloaders, class_names
 
+def get_vgg16_pretrained_model(weights):
+    return None
+
+def eval_model(vgg, criterion):
+    return None
+
+def train_model(vgg, criterion, optimizer, scheduler, num_epochs=10):
+    return None
 
 # Main
-plt.ion()
+
+## Use GPU if available
 use_gpu = torch.cuda.is_available()
 print("[INFO] Using CUDA") if use_gpu else print("[INFO] Using CPU")
+## Define file directories
+file_dir = './data'
+output_dir = './output'
+TRAIN = 'train' 
+VAL = 'val'
+TEST = 'test'
+## Get Data
+datasets_img, datasets_size, dataloaders, class_names = get_data(file_dir, TRAIN, VAL, TEST)
 
-dataloaders, datasets_img, datasets_size = get_data(dir='./data')
