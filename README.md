117ef56a7b190d7dc9b0dec523afecf5b2381a8e
Anh Phan <anhphanvt2705@gmail.com>
2023-07-18 15:35:41 +0700

    add detailed comments

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# Image-Classification
-Image classification project using Pytorch and implement VGG-16 and SVM
+# VGG-16 Image Classification with PyTorch
+
+This repository contains code for training and evaluating an image classification model based on the VGG-16 architecture using PyTorch. It will be able to tell if the input image is a dog or a cat after training.
+
+## Prerequisites
+
+- Python 3.x
+- PyTorch
+- torchvision
+- Matplotlib (optional)
+
+## Getting Started
+
+1. Clone the repository:
+
+   ```shell
+   git clone https://github.com/your-username/Image-Classification-Dog-Cat.git
+   ```
+
+2. Install the required dependencies:
+
+   ```shell
+   pip install torch torchvision matplotlib
+   ```
+
+3. Prepare the Data:
+
+   - Place your training, validation, and test datasets in separate directories (`train`, `val`, `test`) inside the `data` directory.
+
+4. Training and Evaluation:
+
+   - Open the `cat_dog_train_model.py` file and modify the necessary parameters such as file directories, hyperparameters, and number of classes.
+
+   - Run the script:
+
+     ```shell
+     python ./cat_dog_train_model.py
+     ```
+
+   - The script will train the VGG-16 model on the training dataset, evaluate its performance on the validation dataset, and save the trained model for future use.
+
+## Project Structure
+
+The project is organized as follows:
+
+- `vgg16_image_classification.py`: The main script to train and evaluate the VGG-16 model.
+- `data/`: Directory to store the training, validation, and test datasets.
+- `output/`: Directory to save the trained model.
+
+## Code Structure
+
+The code follows the following structure:
+
+- Data Loading and Transformation:
+  - `get_data(file_dir, TRAIN='train', VAL='val', TEST='test')`: Loads and transforms the data using PyTorch's `ImageFolder` and `DataLoader`.
+- Model Creation and Modification:
+  - `get_vgg16_pretrained_model(model_dir='', weights=models.vgg16_bn(pretrained=True).state_dict(), len_target=1000)`: Retrieves the VGG-16 pre-trained model and modifies its classifier for the desired number of output classes.
+- Evaluation:
+  - `eval_model(vgg, criterion, dataset='val')`: Evaluates the model's performance on the specified dataset.
+- Training:
+  - `train_model(vgg, criterion, optimizer, scheduler, num_epochs=10)`: Trains the model using the training dataset and evaluates its performance on the validation dataset.
+
+## Results
+
+The trained VGG-16 model will be saved in the `output` directory as `VGG16_trained.pth`. You can use this model for inference on new images or further fine-tuning if needed.
+
+## Contributing
+
+Contributions are welcome! If you have any suggestions or improvements for this code, please feel free to submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Acknowledgements
+
+- The VGG-16 model implementation is based on the torchvision library in PyTorch.
+- The dataset loading and transformation code is adapted from PyTorch's official documentation.
--- cat_dog_train_model.py
+++ cat_dog_train_model.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
 from torch.autograd import Variable
-# import torchvision
+import torchvision
 from torchvision import datasets, models, transforms
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import time
 import os
 import copy
@@ -17,37 +17,52 @@ TRAIN = 'train'
 VAL = 'val'
 TEST = 'test'
 
-def get_data(file_dir, TRAIN='train', VAL='val', TEST='test'):
+def get_data(file_dir, TRAIN=TRAIN, VAL=VAL, TEST=TEST):
+    """
+    Load and transform the data using PyTorch's ImageFolder and DataLoader.
+
+    Args:
+        file_dir (str): Directory path containing the data.
+        TRAIN (str, optional): Name of the training dataset directory. Defaults to 'train'.
+        VAL (str, optional): Name of the validation dataset directory. Defaults to 'val'.
+        TEST (str, optional): Name of the test dataset directory. Defaults to 'test'.
+
+    Returns:
+        datasets_img (dict): Dictionary containing the datasets for training, validation, and test.
+        datasets_size (dict): Dictionary containing the sizes of the datasets.
+        dataloaders (dict): Dictionary containing the data loaders for training, validation, and test.
+        class_names (list): List of class names.
+    """
     print("[INFO] Loading data...")
-    # Initialize data transformation
+    # Initialize data transformations
     data_transform = {
-        TRAIN : transforms.Compose([
-                transforms.RandomResizedCrop(224),
-                transforms.RandomHorizontalFlip(),
-                transforms.ToTensor()
+        TRAIN: transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor()
         ]),
-        VAL : transforms.Compose([
-                transforms.Resize(256),
-                transforms.CenterCrop(224),
-                transforms.ToTensor()
+        VAL: transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor()
         ]),
-        TEST : transforms.Compose([
-                transforms.Resize(254),
-                transforms.CenterCrop(224),
-                transforms.ToTensor()
+        TEST: transforms.Compose([
+            transforms.Resize(254),
+            transforms.CenterCrop(224),
+            transforms.ToTensor()
         ])
     }
-    # Initilize datasets and transform it
+    # Initialize datasets and apply transformations
     datasets_img = {
-        file : datasets.ImageFolder(
+        file: datasets.ImageFolder(
             os.path.join(file_dir, file),
             transform=data_transform[file]
         )
         for file in [TRAIN, VAL, TEST]
     }
-    # Load data into the desired format
+    # Load data into dataloaders
     dataloaders = {
-        file : torch.utils.data.DataLoader(
+        file: torch.utils.data.DataLoader(
             datasets_img[file],
             batch_size=8,
             shuffle=True,
@@ -55,18 +70,30 @@ def get_data(file_dir, TRAIN='train', VAL='val', TEST='test'):
         )
         for file in [TRAIN, VAL, TEST]
     }
-    # Show result
+    # Get class names and dataset sizes
     class_names = datasets_img[TRAIN].classes
-    datasets_size = {file : len(datasets_img[file]) for file in [TRAIN, VAL, TEST]}
+    datasets_size = {file: len(datasets_img[file]) for file in [TRAIN, VAL, TEST]}
     for file in [TRAIN, VAL, TEST]:
         print(f"[INFO] Loaded {datasets_size[file]} images under {file}")
     print(f"Classes: {class_names}")
-    
+
     return datasets_img, datasets_size, dataloaders, class_names
 
+
 def get_vgg16_pretrained_model(model_dir='', weights=models.VGG16_BN_Weights.DEFAULT, len_target=1000):
+    """
+    Retrieve the VGG-16 pre-trained model and modify its classifier for the desired number of output classes.
+
+    Args:
+        model_dir (str, optional): Directory path for loading a pre-trained model state dictionary. Defaults to ''.
+        weights (str or dict, optional): Pre-trained model weights. Defaults to models.vgg16_bn(pretrained=True).state_dict().
+        len_target (int, optional): Number of output classes. Defaults to 1000.
+
+    Returns:
+        vgg16 (torchvision.models.vgg16): VGG-16 model with modified classifier.
+    """
     print("[INFO] Getting VGG-16 pre-trained model...")
-    # Load VGG-16 pretrained model (1000 features)
+    # Load VGG-16 pretrained model
     if model_dir == '':
         vgg16 = models.vgg16_bn(weights)
     else: 
@@ -74,36 +101,48 @@ def get_vgg16_pretrained_model(model_dir='', weights=models.VGG16_BN_Weights.DEF
     # Freeze training for all layers
     for param in vgg16.features.parameters():
         param.requires_grad = False
-    # Get feature quantity of the last layer
+    # Get number of features in the last layer
     num_features = vgg16.classifier[-1].in_features
     # Remove the last layer
     features = list(vgg16.classifier.children())[:-1]
-    # Add custom layer with custom outputs
+    # Add custom layer with custom number of output classes
     features.extend([nn.Linear(num_features, len_target)])
-    # Replace the model classifier
+    # Replace the model's classifier
     vgg16.classifier = nn.Sequential(*features)
     print("[INFO] Loaded VGG-16 pre-trained model\n", vgg16, "\n")
     return vgg16
 
+
 def eval_model(vgg, criterion, dataset=VAL):
-    print('-'*18)
+    """
+    Evaluate the model's performance on the specified dataset.
+
+    Args:
+        vgg (torchvision.models.vgg16): Model to evaluate.
+        criterion (torch.nn.modules.loss): Loss function.
+        dataset (str, optional): Dataset to evaluate. Defaults to 'val'.
+
+    Returns:
+        avg_loss (float): Average loss on the dataset.
+        avg_accuracy (float): Average accuracy on the dataset.
+    """
+    print('-' * 18)
     print("[Evaluation Model] Evaluating...")
-    # Keeping track
     since = time.time()
     avg_loss = 0
     avg_accuracy = 0
     loss_test = 0
     accuracy_test = 0
-    
+
     batches = len(dataloaders[dataset])
-    # 1 forward pass each data without calculating gradient to find the prediction before training
+    # Perform forward pass on the dataset
     for i, data in enumerate(dataloaders[dataset]):
-        print("\r[Evaluation Model] Evaluate '{}' batch {}/{}".format(dataset, i+1, batches), end='', flush=True)
-            
+        print("\r[Evaluation Model] Evaluate '{}' batch {}/{}".format(dataset, i + 1, batches), end='', flush=True)
+
         vgg.train(False)
         vgg.eval()
         inputs, labels = data
-        
+
         with torch.no_grad():
             if use_gpu:
                 inputs = Variable(inputs.cuda())
@@ -111,32 +150,46 @@ def eval_model(vgg, criterion, dataset=VAL):
             else:
                 inputs = Variable(inputs)
                 labels = Variable(labels)
-                
+
         outputs = vgg(inputs)
-        
+
         _, preds = torch.max(outputs.data, 1)
         loss = criterion(outputs, labels)
-        
+
         loss_test += loss.data
         accuracy_test += torch.sum(preds == labels.data)
-        
+
         # Clear cache to prevent out of memory
         del inputs, labels, outputs, preds
         torch.cuda.empty_cache()
-    
+
     avg_loss = loss_test / datasets_size[dataset]
     avg_accuracy = accuracy_test / datasets_size[dataset]
-    
+
     elapsed_time = time.time() - since
     print()
     print(f"[Evaluation Model] Evaluation completed in {(elapsed_time // 60):.0f}m {(elapsed_time % 60):.0f}s")
     print(f"[Evaluation Model] Avg loss      ({dataset}): {avg_loss:.4f}")
     print(f"[Evaluation Model] Avg accuracy  ({dataset}): {avg_accuracy:.4f}")
-    print('-'*18)
+    print('-' * 18)
     return avg_loss, avg_accuracy
 
+
 def train_model(vgg, criterion, optimizer, scheduler, num_epochs=10):
-    print('\n','#'*15, ' TRAINING ', '#'*15, '\n')
+    """
+    Train the model using the training dataset and evaluate its performance on the validation dataset.
+
+    Args:
+        vgg (torchvision.models.vgg16): Model to train.
+        criterion (torch.nn.modules.loss): Loss function.
+        optimizer (torch.optim): Optimizer for model parameter updates.
+        scheduler (torch.optim.lr_scheduler): Learning rate scheduler.
+        num_epochs (int, optional): Number of epochs to train. Defaults to 10.
+
+    Returns:
+        vgg (torchvision.models.vgg16): Trained model.
+    """
+    print('\n', '#' * 15, ' TRAINING ', '#' * 15, '\n')
     print('[TRAIN MODEL] Training...')
     since = time.time()
     best_model_wts = copy.deepcopy(vgg.state_dict())
@@ -145,61 +198,62 @@ def train_model(vgg, criterion, optimizer, scheduler, num_epochs=10):
     avg_accuracy = 0
     avg_loss_val = 0
     avg_accuracy_val = 0
-    
+
     train_batches = len(dataloaders[TRAIN])
-    
+
     for epoch in range(num_epochs):
         print('')
-        print(f"[TRAIN MODEL] Epoch {epoch+1}/{num_epochs}")
+        print(f"[TRAIN MODEL] Epoch {epoch + 1}/{num_epochs}")
         loss_train = 0
         accuracy_train = 0
         vgg.train(True)
-        
+
         for i, data in enumerate(dataloaders[TRAIN]):
-            print("\r[TRAIN MODEL] Training batch {}/{}".format(i+1, train_batches // 2), end='', flush=True)
-            # Use half training dataset
+            print("\r[TRAIN MODEL] Training batch {}/{}".format(i + 1, train_batches // 2), end='', flush=True)
+            # Use only half of the training dataset
             if i >= train_batches // 2:
                 break
             inputs, labels = data
+            
             # Forward pass
             if use_gpu:
                 inputs = Variable(inputs.cuda())
                 labels = Variable(labels.cuda())
             else:
                 inputs = Variable(inputs)
-                labels = Variable(labels)   
+                labels = Variable(labels)
             optimizer.zero_grad()
             outputs = vgg(inputs)
             _, preds = torch.max(outputs.data, 1)
-            
-            # Back propagation
+
+            # Backward propagation
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
-            
-            # Save result
+
+            # Save results
             loss_train += loss.data
             accuracy_train += torch.sum(preds == labels.data)
-            
+
             # Clear cache
             del inputs, labels, outputs, preds
             torch.cuda.empty_cache()
-            
+
         avg_loss = loss_train * 2 / datasets_size[TRAIN]
-        avg_accuracy = accuracy_train * 2 /datasets_size[TRAIN]
+        avg_accuracy = accuracy_train * 2 / datasets_size[TRAIN]
         vgg.train(False)
         vgg.eval()
         print('')
         avg_loss_val, avg_accuracy_val = eval_model(vgg, criterion, dataset=VAL)
-        
-        print('-'*13)
-        print(f"[TRAIN MODEL] Epoch {epoch+1} result: ")
+
+        print('-' * 13)
+        print(f"[TRAIN MODEL] Epoch {epoch + 1} result: ")
         print(f"[TRAIN MODEL] Avg loss      (train):    {avg_loss:.4f}")
         print(f"[TRAIN MODEL] Avg accuracy  (train):    {avg_accuracy:.4f}")
         print(f"[TRAIN MODEL] Avg loss      (val):      {avg_loss_val:.4f}")
         print(f"[TRAIN MODEL] Avg accuracy  (val):      {avg_accuracy_val:.4f}")
-        print('-'*13)
-        
+        print('-' * 13)
+
         if avg_accuracy_val > best_accuracy:
             best_accuracy = avg_accuracy_val
             best_model_wts = copy.deepcopy(vgg.state_dict())
@@ -207,11 +261,12 @@ def train_model(vgg, criterion, optimizer, scheduler, num_epochs=10):
     elapsed_time = time.time() - since
     print(f"[TRAIN MODEL] Training completed in {(elapsed_time // 60):.0f}m {(elapsed_time % 60):.0f}s")
     print(f"[TRAIN MODEL] Best accuracy: {best_accuracy:.4f}")
-    print('\n','#'*15, ' FINISHED ', '#'*15, '\n')
+    print('\n', '#' * 15, ' FINISHED ', '#' * 15, '\n')
     vgg.load_state_dict(best_model_wts)
     return vgg
 
-if __name__ ==  '__main__':
+
+if __name__ == '__main__':
     # Use GPU if available
     use_gpu = torch.cuda.is_available()
     print("[INFO] Using CUDA") if use_gpu else print("[INFO] Using CPU")
@@ -228,10 +283,11 @@ if __name__ ==  '__main__':
     optimizer_ft = optim.SGD(vgg16.parameters(), lr=1e-3, momentum=0.9)
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=1)
     # Evaluate before training
-    print("[INFO] Before training evalutaion in progress...")
+    print("[INFO] Before training evaluation in progress...")
     eval_model(vgg16, criterion, dataset=TEST)
+    # Training
     vgg16 = train_model(vgg16, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=2)
     torch.save(vgg16.state_dict(), output_dir)
-    print("[INFO] After training evalutaion in progress...")
+    # Evaluate after training
+    print("[INFO] After training evaluation in progress...")
     eval_model(vgg16, criterion, dataset=TEST)
-    
