ea227b8c3596a891e3d83aa54b8b05e64c68fec0
Anh Phan <anhphanvt2705@gmail.com>
2023-07-21 23:21:04 +0700

    minor change

--- cat_dog_train_model.py
+++ cat_dog_train_model.py
@@ -391,7 +391,7 @@ if __name__ == '__main__':
     datasets_img, datasets_size, dataloaders, class_names = get_data(file_dir)
     # Get VGG16 pre-trained model
     vgg16 = get_vgg16_pretrained_model(len_target=2)
-    # vgg16 = get_vgg16_pretrained_model('./output/VGG16_trained.pth', len_target=2)      # If load custom pre-trained model, watch out to match len target
+    # vgg16 = get_vgg16_pretrained_model(model_dir='./output/VGG16_trained.pth', len_target=2)      # If load custom pre-trained model, watch out to match len target
     # Move model to GPU
     if use_gpu:
         torch.cuda.empty_cache()
