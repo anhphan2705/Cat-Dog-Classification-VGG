33b9d2295eed4eb19fd2037da88fc17f48403d57
Anh Phan <anhphanvt2705@gmail.com>
2023-07-23 23:00:42 +0700

    provide correct output

--- cat-dog-classifier-api.py
+++ cat-dog-classifier-api.py
@@ -14,6 +14,7 @@ import time
 
 app = FastAPI()
 MODEL_DIRECTORY = './output/VGG16_trained_9960.pth'
+use_gpu = torch.cuda.is_available()
 
 
 def convert_byte_to_arr(byte_image):
@@ -50,6 +51,7 @@ def convert_arr_to_byte(arr_image):
 
 def get_data(np_images):
     data_transform = transforms.Compose([
+        transforms.ToPILImage(), 
         transforms.Resize(254),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
@@ -61,9 +63,14 @@ def get_data(np_images):
     #         shuffle=False,
     #         num_workers=1
     #     )
-
-    data = data_transform(np_images)
-    data = data.unsqueeze(0)
+    data = []
+    for image in np_images:
+        # Expand to [batch_size, h, w, d]
+        image = data_transform(image)
+        image = torch.unsqueeze(image, 0)
+        print('inside1', image)
+        print('inside2', image.shape)
+        data.append(image)
     return data
 
 def get_vgg16_pretrained_model(model_dir = MODEL_DIRECTORY , weights=models.VGG16_BN_Weights.DEFAULT):
@@ -79,13 +86,51 @@ def get_vgg16_pretrained_model(model_dir = MODEL_DIRECTORY , weights=models.VGG1
         vgg16 (torchvision.models.vgg16): VGG-16 model with modified classifier.
     """
     print("[INFO] Getting VGG-16 pre-trained model...")
-    # Load VGG-16 pretrained model
     vgg16 = models.vgg16_bn(weights)
+    # Freeze training for all layers
+    for param in vgg16.features.parameters():
+        param.requires_grad = False
+    # Get number of features in the last layer
+    num_features = vgg16.classifier[-1].in_features
+    # Remove the last layer
+    features = list(vgg16.classifier.children())[:-1]
+    # Add custom layer with custom number of output classes
+    features.extend([nn.Linear(num_features, 2)])
+    # Replace the model's classifier
+    vgg16.classifier = nn.Sequential(*features)
+    # Load VGG-16 pretrained model
     vgg16.load_state_dict(torch.load(model_dir))
     vgg16.eval()
     print("[INFO] Loaded VGG-16 pre-trained model\n", vgg16, "\n")
     
     return vgg16
+
+def get_prediction(model, images):
+    since = time.time()
+    labels = []
+    model.train(False)
+    model.eval()
+    
+    for image in images:
+        with torch.no_grad():
+            if use_gpu:
+                image = Variable(image.cuda())
+            else:
+                image = Variable(image)
+                
+        outputs = model(image)
+        _, pred = torch.max(outputs.data, 1)
+        
+        if pred == 0:
+            labels.append('cat')
+        elif pred == 1:
+            labels.append('dog')
+        else:
+            print('[INFO] Labeling went wrong')
+    
+    elapsed_time = time.time() - since
+    
+    return labels, elapsed_time
 
 
 @app.get("/")
@@ -118,24 +163,36 @@ async def dog_cat_classification(in_images: list[UploadFile]):
     Raises:
         Exception: If stitching fails or the image cannot be cropped.
     """
+    print("begin")
     images = []
     for in_image in in_images:
         byte_image = await in_image.read()
         arr_image = convert_byte_to_arr(byte_image)
         images.append(arr_image)
+        
+    for image in images:
+        print('1', type(image))
     # Preping data n model
     data = get_data(images)
+    for img in data:
+        print('2', type(img))
     vgg = get_vgg16_pretrained_model()
     # Use GPU if available
-    use_gpu = torch.cuda.is_available()
     print("[INFO] Using CUDA") if use_gpu else print("[INFO] Using CPU")
     if use_gpu:
         torch.cuda.empty_cache()
         vgg.cuda()
+    labels, elapsed_time = get_prediction(vgg, data)
+    print(labels)
+    print(elapsed_time)
 
-    byte_image = convert_arr_to_byte(images)
-    return Response(byte_image, media_type="image/jpg")
+    # byte_image = convert_arr_to_byte(filled_img)
 
+    # response_text = f'Label : {labels} in time {elapsed_time}s'
+
+    # response = Response(content=byte_image, media_type="image/jpg")
+    # response.headers["Result"] = response_text
+    # return response
 
 if __name__ == "__main__":
     import uvicorn
